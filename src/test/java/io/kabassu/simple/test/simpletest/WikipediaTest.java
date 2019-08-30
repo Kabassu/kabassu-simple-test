5de1e16ae10adc2599dc864a63e0fcb2beb05bc4
Daniel Madejek <daniel.madejek@cognifide.com>
2019-08-30 10:30:59 +0200

    simple test

---
+++ src/test/java/io/kabassu/simple/test/simpletest/WikipediaTest.java
@@ -0,0 +1,33 @@
+package io.kabassu.simple.test.simpletest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import com.cognifide.qa.bb.page.BobcatPageFactory;
+import com.google.inject.Inject;
+import org.junit.jupiter.api.Test;
+import com.cognifide.qa.bb.junit5.guice.Modules;
+
+import com.cognifide.qa.bb.modules.BobcatRunModule;
+import io.kabassu.simple.test.simpletest.pageobjects.WikipediaPage;
+import io.kabassu.simple.test.simpletest.pageobjects.DefinitionPage;
+
+@Modules(BobcatRunModule.class)
+public class WikipediaTest {
+
+  private static final String SEARCH_QUERY = "hello world";
+  private static final String HEADING = "\"Hello, World!\" program";
+
+  @Inject
+  private BobcatPageFactory bobcatPageFactory;
+
+  @Inject
+  private DefinitionPage definitionPage;
+
+  @Test
+  public void wikipediaSearchTest() {
+    WikipediaPage homePage = bobcatPageFactory.create("https://en.wikipedia.org", WikipediaPage.class);
+    homePage.open().getSearchComponent().searchForQuery(SEARCH_QUERY);
+    assertThat(definitionPage.getHeading(), is(HEADING));
+  }
+}
